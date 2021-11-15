a7053902a3f1604bb25705c5317885cc15e9bfaa
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-15 11:42:27 +0530

    add program to reverse a string using stack

---
+++ src/stack/ReverseString.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println(reverseString("ABCDE"));
+    }
+
+    public static String reverseString(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            stack.push(c);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = stack.pop();
+        }
+        return new String(chars);
+    }
+}
